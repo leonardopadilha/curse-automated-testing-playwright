93e7cfa1d5ef7c27d51147eac7eec1e54c1c8bd5
leonardopadilha <leonardopadilha28@gmail.com>
2023-06-29 06:23:01 -0300

    Inputs and Assertions

--- modulo02-Playwright-Fundamentals/tests/example.spec.ts
+++ modulo02-Playwright-Fundamentals/tests/example.spec.ts
@@ -15,6 +15,33 @@ test("Clickin on Elements", async ({ page }) => {
     await expect(errorMessage).toContainText("Login and/or password are wrong.")
 })
 
+test("Working with inputs", async ({ page }) => {
+    await page.goto("http://zero.webappsecurity.com/index.html")
+    await page.click("#signin_button")
+
+    await page.type("input[type='text']", "wrongUser")
+    await page.type("#user_password", "wrongPassword")
+
+    await page.click("text=Sign in")
+
+    const errorMessage = await page.locator(".alert-error")
+    await expect(errorMessage).toContainText("Login and/or password are wrong.")
+})
+
+test("Assertions", async ({ page }) => {
+    await page.goto("https://example.com/")
+    await expect(page).toHaveURL("https://example.com/")
+    await expect(page).toHaveTitle("Example Domain")
+
+    const element = await page.locator("h1")
+    await expect(element).toBeVisible()
+    await expect(element).toHaveText("Example Domain")
+    await expect(element).toHaveCount(1)
+
+    const notExistingElement = await page.locator("h5")
+    await expect(notExistingElement).not.toBeVisible()
+})
+
 test.skip("Selectors", async ({ page }) => {
     // text
     await page.click("text=some text")
