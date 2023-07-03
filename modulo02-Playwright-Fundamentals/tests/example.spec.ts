b121edf18c4d8df94dece1447d9df0debcb938f1
leonardopadilha <leonardopadilha28@gmail.com>
2023-07-03 06:32:22 -0300

    Custom Helpers

--- modulo02-Playwright-Fundamentals/tests/example.spec.ts
+++ modulo02-Playwright-Fundamentals/tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test'
+import { loadHomepage, assertTitle } from '../helpers'
 
 test("Simple basic test", async ({ page }) => {
     await page.goto("https://example.com/")
@@ -59,15 +60,27 @@ test.describe("My first test suite", () => {
     })
 })
 
-test('Screenshots', async({ page }) => {
-    await page.goto('https://example.com/')
-    await page.screenshot({ path:'screenshots/myScreenshot.png' });
+test.describe("Hooks", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://example.com/')
+    })
+
+    test('Screenshots', async({ page }) => {
+        //await page.goto('https://example.com/')
+        await page.screenshot({ path:'screenshots/myScreenshot.png' });
+    })
+    
+    test('Single element screenshot', async ({ page }) => {
+        //await page.goto('https://example.com/')
+        const element = await page.locator('h1')
+        await element.screenshot({ path:'screenshots/single_element_screenshot.png' })
+    })
 })
 
-test('Single element screenshot', async ({ page }) => {
-    await page.goto('https://example.com/')
-    const element = await page.locator('h1')
-    await element.screenshot({ path:'screenshots/single_element_screenshot.png' })
+test("Custom Helpers", async ({ page }) => {
+    await loadHomepage(page)
+    await page.pause() // Open inspector
+    await assertTitle(page)
 })
 
 test.skip("Selectors", async ({ page }) => {
