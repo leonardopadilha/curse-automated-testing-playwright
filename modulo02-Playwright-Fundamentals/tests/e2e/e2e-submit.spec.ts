02bcd306b304bb0eb64818d1a63dcf4d68c46eb5
leonardopadilha <leonardopadilha28@gmail.com>
2023-07-06 06:49:02 -0300

    Feedback form - Reset feedback form

---
+++ modulo02-Playwright-Fundamentals/tests/e2e/e2e-submit.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test"
+
+test.describe("Feedback form", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("http://zero.webappsecurity.com/index.html")
+        await page.click("#feedback")
+    })
+
+    test("Reset feedback form", async ({ page }) => {
+        const message = "Some nice comment about the application"
+
+        await page.type('input[name="name"]', 'user')
+        await page.type('input[placeholder="Your email address"]', 'email@email.com')
+        await page.type('#subject', 'new user')
+        await page.type('#comment', message)
+
+        await page.click('input[name="clear"]')
+        
+        const nameInput = await page.locator('input[name="name"]')
+        const commentInput = await page.locator('#comment')
+        await expect(nameInput).toBeVisible()
+        await expect(commentInput).toBeVisible()
+    })
+})
