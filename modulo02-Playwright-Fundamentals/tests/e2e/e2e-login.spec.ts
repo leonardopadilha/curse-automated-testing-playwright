1944be9f49f79b7de4e3fe9f7d1777932fa68dd2
leonardopadilha <leonardopadilha28@gmail.com>
2023-07-07 05:50:47 -0300

    Positive Scenario for login

--- modulo02-Playwright-Fundamentals/tests/e2e/e2e-login.spec.ts
+++ modulo02-Playwright-Fundamentals/tests/e2e/e2e-login.spec.ts
@@ -16,4 +16,19 @@ test.describe('Login / Logout Flow', () => {
         const message = await page.locator(".alert-error")
         await expect(message).toContainText("Login and/or password are wrong.")
     })
+
+    test("Positive Scenario for login", async ({ page }) => {
+        await page.click("button[type='button']")
+
+        await page.type("#user_login", "username")
+        await page.type("#user_password", "password")
+        await page.getByLabel("Keep me signed in").check()
+        await page.click(".form-actions > input")
+
+        await page.goto("http://zero.webappsecurity.com/bank/transfer-funds.html")
+        
+        const accountSumary = await page.locator("#account_summary_tab > a")
+        await expect(accountSumary).toBeVisible()
+        await expect(accountSumary).toContainText("Account Summary")
+    })
 });
