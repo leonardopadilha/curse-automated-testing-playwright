8489b0a99f6a567506accc08a6707775986aef78
leonardopadilha <leonardopadilha28@gmail.com>
2023-07-08 07:20:39 -0300

    Submit feedback form

--- modulo02-Playwright-Fundamentals/tests/e2e/e2e-submit.spec.ts
+++ modulo02-Playwright-Fundamentals/tests/e2e/e2e-submit.spec.ts
@@ -21,4 +21,18 @@ test.describe("Feedback form", () => {
         await expect(nameInput).toBeVisible()
         await expect(commentInput).toBeVisible()
     })
+
+    test("Submit feedback form", async ({ page }) => {
+        const message = "Some nice comment about the application"
+
+        await page.type('input[name="name"]', 'user')
+        await page.type('input[placeholder="Your email address"]', 'email@email.com')
+        await page.type('#subject', 'new user')
+        await page.type('#comment', message)
+
+        await page.click('input[value="Send Message"]')
+
+        await page.waitForSelector("#feedback-title")
+        await expect(page).toHaveURL("http://zero.webappsecurity.com/sendFeedback.html")
+    })
 })
