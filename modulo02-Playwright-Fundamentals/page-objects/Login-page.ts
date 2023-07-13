8316f03f6af7e4116a106e18c52ddb073a143954
leonardopadilha <leonardopadilha28@gmail.com>
2023-07-13 06:09:00 -0300

    Add snapshot with page objects

--- modulo02-Playwright-Fundamentals/page-objects/Login-page.ts
+++ modulo02-Playwright-Fundamentals/page-objects/Login-page.ts
@@ -7,6 +7,7 @@ export class LoginPage extends AbstractPage {
     readonly passwordInput: Locator
     readonly submitButton: Locator
     readonly errorMessage: Locator
+    readonly loginForm: Locator
 
     constructor(page: Page) {
         //this.page = page
@@ -15,6 +16,7 @@ export class LoginPage extends AbstractPage {
         this.passwordInput = page.locator("#user_password")
         this.submitButton = page.locator("text=Sign in")
         this.errorMessage = page.locator(".alert-error")
+        this.loginForm = page.locator('#login_form')
     }
 
     async erroPage() {
@@ -29,5 +31,13 @@ export class LoginPage extends AbstractPage {
 
     async assertErrorMessage(message: string) {
         await expect(this.errorMessage).toContainText(message)
+    }
+
+    async snapshotLoginForm() {
+        await expect(await this.loginForm.screenshot()).toMatchSnapshot('login-form.png')
+    }
+
+    async snapshotErrorMessage() {
+        await expect(await this.errorMessage.screenshot()).toMatchSnapshot('login-error.png')
     }
 }
