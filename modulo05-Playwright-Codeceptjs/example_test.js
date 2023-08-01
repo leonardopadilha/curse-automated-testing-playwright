1f5f442f71294cec0180e902ac4be57c063c63ef
leonardopadilha <leonardopadilha28@gmail.com>
2023-08-01 06:17:57 -0300

    Create and run first test

--- modulo05-Playwright-Codeceptjs/example_test.js
+++ modulo05-Playwright-Codeceptjs/example_test.js
@@ -1,5 +1,6 @@
 Feature('example');
 
-Scenario('test something',  ({ I }) => {
-
+Scenario('Should load example.com website',  ({ I }) => {
+    I.amOnPage("https://example.com/")
+    I.see("Example")
 });
