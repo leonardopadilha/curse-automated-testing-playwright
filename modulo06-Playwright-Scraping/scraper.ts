909a0b0c687212099594658f5f2233d99f8ba168
leonardopadilha <leonardopadilha28@gmail.com>
2023-08-04 06:20:15 -0300

    Get Data from WebSites

--- modulo06-Playwright-Scraping/scraper.ts
+++ modulo06-Playwright-Scraping/scraper.ts
@@ -1,15 +1,38 @@
 const playwright = require('playwright')
+const random_useragent = require('random-useragent')
 
 const BASE_URL="https://github.com/topics/playwright"
 
 ;(async () => {
+    // Create Random Agent
+    const agent = random_useragent.getRandom()
+
     //Setup Browser
     const browser = await playwright.chromium.launch({ headless: true })
-    const context = await browser.newContext()
+    const context = await browser.newContext({ userAgent: agent })
     const page = await context.newPage({ bypassCSP: true })
     await page.setDefaultTimeout(30000)
     await page.setViewportSize({ width: 800, height: 600 })
     await page.goto(BASE_URL)
+
+    // Get Data from Website
+    const repositories = await page.$$eval('article.border', (repoCards) => {
+        return repoCards.map((card) => {
+            const [user, repo] = card.querySelectorAll('h3 a')
+
+            const formatText = (element) => element && element.innerText.trim()
+
+            return {
+                user: formatText(user),
+                repo: formatText(repo),
+                url: repo.href,
+            }
+        })
+    })
+
+    console.log(repositories)
+
+    // Store Data into File
 
     // Close browser
     await browser.close()
@@ -18,3 +41,5 @@ const BASE_URL="https://github.com/topics/playwright"
     console.log(error)
     process.exit(1)
 })
+
+// npm install random-useragent
