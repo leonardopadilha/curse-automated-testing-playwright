ac73acd6fefcabe37e9ad51383f406adcf8dc7ce
leonardopadilha <leonardopadilha28@gmail.com>
2023-08-06 09:15:03 -0300

    REST API Testing

---
+++ modulo02-Playwright-Fundamentals/tests/api/api.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test"
+
+test.describe.parallel("API Testing", () => {
+    const baseUrl = "https://reqres.in/api"
+
+    test("Simple API Test - Assert Response Status", async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/2`)
+        expect(response.status()).toBe(200)
+    })
+
+    test("Simple API Test - Assert Invalid Endpoint", async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/non-existing-endpoint`)
+        expect(response.status()).toBe(404)
+    })
+
+    test("Simple API Test with JSON data - Assert Response Status", async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/2`)
+        expect(response.status()).toBe(200)
+
+        const responseBody = JSON.parse(await response.text())
+        console.log(responseBody)
+    })
+})
