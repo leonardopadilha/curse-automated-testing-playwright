762757d1d3c6c82896364f103b1cec3b8f72e7bc
leonardopadilha <leonardopadilha28@gmail.com>
2023-08-07 13:11:19 -0300

    GET Request Test

--- modulo02-Playwright-Fundamentals/tests/api/api.spec.ts
+++ modulo02-Playwright-Fundamentals/tests/api/api.spec.ts
@@ -14,10 +14,22 @@ test.describe.parallel("API Testing", () => {
     })
 
     test("Simple API Test with JSON data - Assert Response Status", async ({ request }) => {
-        const response = await request.get(`${baseUrl}/users/2`)
+        const response = await request.get(`${baseUrl}/users/1`)
         expect(response.status()).toBe(200)
 
         const responseBody = JSON.parse(await response.text())
         console.log(responseBody)
+    })
+
+    test("GET Request - Get User Detail", async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/1`)
+        const responseBody = JSON.parse(await response.text())
+
+        expect(response.status()).toBe(200)
+        expect(responseBody.data.id).toBe(1)
+        expect(responseBody.data.first_name).toEqual('George')
+        expect(responseBody.data.last_name).toBe('Bluth')
+        expect(responseBody.data.email).toBeTruthy()
+        //console.log(responseBody)
     })
 })
